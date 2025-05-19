690fc8216076fce997aa87b208065570f70a0ba7
Astar <bahouidi99@gmail.com>
2025-05-19 11:40:27 +0100

    Crafted ch03 Makefile(s)

--- ch03-object-files/ExtremeC_examples_chapter3_4.c
+++ ch03-object-files/ExtremeC_examples_chapter3_4.c
@@ -1,30 +1,28 @@
 // File name: ExtremeC_examples_chapter3_4.c
 // Description: Load the shared object file using dlopen
-// NOTE: Why dlclose Is Important:
-//  Resource Management: It releases resources allocated for the shared library
-//  by the operating system.
-//  Avoid Memory Leaks: Without dlclose, the memory used
-//  for the shared library remains occupied until the program terminates.
-
+// NOTE: Why dlclose Is Important: (1) Resource Management: It releases
+// resources allocated for the shared library by the operating system. (2) Avoid
+// Memory Leaks: Without dlclose, the memory used for the shared library remains
+// occupied until the program terminates.
 #include <dlfcn.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "ExtremeC_examples_chapter3_2_geometry.h"
 
 int main(void) {
-
+  // Lazy load dll (dynamically loaded library) of shared object files
   void *handle = dlopen("./opt/geometry/libgeometry.so", RTLD_LAZY);
   if (!handle) {
     fprintf(stderr, "%s\n", dlerror());
     exit(EXIT_FAILURE);
   }
-
+  // Retrieve the relevant function
   polar_pos_3d_t (*func_ptr)(cartesian_pos_3d_t *);
   func_ptr = dlsym(handle, "convert_to_3d_polar_pos");
   if (!func_ptr) {
     fprintf(stderr, "%s\n", dlerror());
-    dlclose(handle); // Close the library before exiting
+    // Close the library before exiting
+    dlclose(handle);
     exit(EXIT_FAILURE);
   }
 
@@ -38,7 +36,6 @@ int main(void) {
   printf("Polar position:\n\t%6s: %f\n\t%6s: %f (deg)\n\t%6s: %f (deg)\n",
          "Length", polar_pos.length, "Theat", polar_pos.theta, "Phi",
          polar_pos.phi);
-
   // Close the library handle
   dlclose(handle);
   return EXIT_SUCCESS;
